cdbfec32da607caa19da4056ee33bc90e234ce72
Adriano Maciel <adriano.maciel@simplebrasil.com>
2015-01-14 14:00:09 -0200

    - Adicionados binds para status da conexão; - Adicionado aviso ao usuário quando a conexão for perdida;

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,12 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+
+        //armazena o status atual da conexão, inicia como desconectado
+        this.possuiConexao = false;
+
+        //armazena o tipo de conexão
+        this.tipoConexao = '';
     },
     // Bind Event Listeners
     //
@@ -27,6 +33,9 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+
+        document.addEventListener('online', this.checkConnection, false); //escutando quando o dispositivo ficar online
+        document.addEventListener('offline', this.checkConnection, false);  //escutando quando o dispositivo ficar offline
     },
     // deviceready Event Handler
     //
@@ -45,5 +54,35 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    checkConnection: function() {
+        //recupera o status atual da conexão
+        var networkState = navigator.connection.type;
+
+        //criando um objeto com o significado de cada status de conexão
+        var states = {};
+        states[networkState.UNKNOWN]  = 'Unknown connection';
+        states[networkState.ETHERNET] = 'Ethernet connection';
+        states[networkState.WIFI]     = 'WiFi connection';
+        states[networkState.CELL_2G]  = 'Cell 2G connection';
+        states[networkState.CELL_3G]  = 'Cell 3G connection';
+        states[networkState.CELL_4G]  = 'Cell 4G connection';
+        states[networkState.NONE]     = 'No network connection';
+
+        //se não tiver conexão, zerar as variáveis
+        if( networkState == 'unknown' || networkState == 'none' )
+        {
+            this.possuiConexao = false;
+            this.tipoConexao = '';
+
+            window.plugins.toast.showLongBottom('Desculpe, mas é necessário ter conexão com a internet para utilizar o aplicativo.');
+            navigator.notification.vibrate(1000);
+        }
+        else
+        {
+            //armazenando nas variáveis globais
+            this.possuiConexao = true;
+            this.tipoConexao = networkState;
+        }
     }
 };
