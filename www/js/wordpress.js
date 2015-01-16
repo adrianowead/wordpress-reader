67991b4c47b309977f4b85c225992b0d2918c30c
Adriano Maciel <adriano.maciel@simplebrasil.com>
2015-01-16 16:04:32 -0200

    - Adicionado template do menu principal;

--- www/js/wordpress.js
+++ www/js/wordpress.js
@@ -16,7 +16,13 @@ var wp = {
 			url: this.url + 'taxonomies/category/terms',
 			type: 'get',
 			success: function(data){
-				console.log(data);
+				//em caso de sucesso, carregar o layout do menu principal
+				//carregando layout
+				data = {'supplies':data};
+				var html = new EJS({url: 'template/menu-principal.ejs'}).render(data);
+
+				//adicionando ao final do html atual
+				$("#app").append( html );
 			},
 			error: function(){
 				window.plugins.toast.showLongBottom('Desculpe, mas não foi possível carregar as categorias, verifique sua conexão com a internet e tente novamente.');
